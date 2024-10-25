bd5bc4ab42a6ac049fb36970facfe679c6374def
Alejandra Lasso <e1719255430@uisrael.edu.ec>
2024-10-24 23:37:19 -0500

    alassoExamen

--- Views/Registro.xaml.cs
+++ Views/Registro.xaml.cs
@@ -33,10 +33,10 @@ public partial class Registro : ContentPage
 
     private void Button_Clicked_1(object sender, EventArgs e)
     {
-        string fechaSeleccionada = fechaPicker.Date.ToString("d"); // Formato corto de fecha
+        string fechaSeleccionada = fechaPicker.Date.ToString("d"); 
         string pais = paisPicker.SelectedItem?.ToString();
         string ciudad = cuidadPicker.SelectedItem?.ToString();
-        double montoInicial = Convert.ToDouble(montoInicialEntry.Text); // Convertir monto inicial a double
+        double montoInicial = Convert.ToDouble(montoInicialEntry.Text); 
         string nombre = nombreEntry.Text;
         string apellido = apellidoEntry.Text;
         string edad = edadEntry.Text;
@@ -47,10 +47,8 @@ public partial class Registro : ContentPage
             double saldoRestante = costoTotal - montoInicial;
             double pagoMensual2 = (saldoRestante / 4) * 1.04;
 
-            // Convertir pagoMensual2 a string para mostrarlo y pasarlo
             pagoMensualEntry.Text = pagoMensual2.ToString("F2");
 
-            // Convertir montoInicial y pagoMensual2 a string al pasarlos a Resumen
             Navigation.PushAsync(new Resumen(user_global, fechaSeleccionada, pais, ciudad, montoInicial.ToString("F2"), pagoMensual2.ToString("F2"), nombre, apellido, edad));
         }
         else
